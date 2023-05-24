57f86bbd38aae97b22f788ce924be9264c3553f7
clown <windows>
2023-05-24 17:27:15 +0300

    adding app icon

--- app/src/main/java/com/example/diplom2/MainActivity.kt
+++ app/src/main/java/com/example/diplom2/MainActivity.kt
@@ -8,6 +8,7 @@ import com.yandex.mapkit.MapKitFactory
 
 //TODO: Проложить путь
 //TODO: Check scan
+//TODO: Add image icon
 
 class MainActivity : AppCompatActivity() {
 //    private fun addToDatabase() {
